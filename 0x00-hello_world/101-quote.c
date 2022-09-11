a69d817d7da0b7d3e9cf727b457a414e5ccf2faf
deliicmugambi <deliicmugambi@gmail.com>
2022-09-11 12:57:19 +0000

    updated 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
+/**
+ * main - prints the string followed by a new line to the standrad error
+ *
+ * Return: returns 1
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
