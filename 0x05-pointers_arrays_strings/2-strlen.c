bf723162c9d485f39181cc1e10b7335cf8af804f
deliicmugambi <deliicmugambi@gmail.com>
2022-09-19 20:49:04 +0000

    fixed task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,13 @@
  *@s: pointer to a string to find its length
  *Return: int - the length of a string
  */
-
-int strlen(char *s)
+int _strlen(char *s)
 {
-	int len = 0, i = 0;
-	while (s[i] ! = '\0')
-	{
-		len += 1;
-		i++;
-
-	}
+int len = 0, i = 0;
+while (s[i] != '\0')
+{
+len += 1;
+i++;
+}
 return (len);
 }
